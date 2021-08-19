03e0bacfe9aeff4c86523ae9a8d04c2e7ca94eed
SteveoBurns <steveo.burns.777@gmail.com>
2021-08-19 15:05:28 +1000

    Added UI and music

--- Assets/Kamikaze Thunder Pilots/Scripts/Steve/EventManager.cs
+++ Assets/Kamikaze Thunder Pilots/Scripts/Steve/EventManager.cs
@@ -1,5 +1,4 @@
 using Game.Cleon;
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,49 +10,77 @@ namespace Game.Steveo
     
     public class EventManager : MonoBehaviour
     {
+        [Header("Scroll Elements")]
         [SerializeField] private GameObject scrollPrefab;
         [SerializeField] private Text scrollText;
-        [SerializeField] private AudioSource caughtSFX;
+        [SerializeField] private Text continueText;
+        [SerializeField] private Text tryAgainText;
         [SerializeField, TextArea] private string caughtText;
-        [SerializeField] private CharacterMovement player;
+        [SerializeField, TextArea] private string startText;
         
-        private void Awake()
-        {
-            
-        }
+        [Header("Sound Effects")]
+        [SerializeField] private AudioSource caughtSFX;
+        
+        [Header("Player Character")]
+        [SerializeField] private CharacterMovement player;
 
+        private static bool isRestart = false; 
+        
+        
         // Start is called before the first frame update
         void Start()
         {
             SightCone.Caught += Caught;
-            if(scrollPrefab != null)
-                scrollPrefab.SetActive(false);
-           
+
+            if(!isRestart)
+            {
+               ShowScroll(startText);
+               tryAgainText.enabled = false;
+            }
+
+            isRestart = true;
             Time.timeScale = 1;
-        
+
         }
 
+        private void ShowScroll(string _text)
+        {
+            if(scrollPrefab != null)
+                scrollPrefab.SetActive(true);
+            scrollText.text = _text;
+            Time.timeScale = 0;
+            if(player != null)
+                player.enabled = false;
+        }
+
+        public void Play()
+        {
+            player.enabled = true;
+            Time.timeScale = 1;
+            scrollPrefab.SetActive(false);
+
+        }
+
+        /// <summary>
+        /// Gets called when caught by the watchers sight cones.
+        /// </summary>
         public void Caught()
         {
             if(caughtSFX != null)
             {
+                // Play the caught sfx
                 caughtSFX.Play();
             }
 
-            Time.timeScale = 0;
-            if(scrollPrefab != null)
+            ShowScroll(caughtText);
+            if(continueText != null && tryAgainText != null)
             {
-                scrollPrefab.SetActive(true);
-                scrollText.text = caughtText;
-                player.enabled = false;
+                continueText.enabled = false;
+                tryAgainText.enabled = true;
             }
-            
+                
+                
         }
-        
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+
     }
 }
